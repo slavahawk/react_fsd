a3faa514555664e1a01f6e4602ac4162a84678c7
Slava <krylov_s10@mail.ru>
2023-10-02 11:01:46 +0700

    add render store, add first entity

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import { render } from 'react-dom';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { BrowserRouter } from 'react-router-dom';
 import './app/styles/index.scss';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import App from './app/App';
-
 import './shared/config/i18n/i18n';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
